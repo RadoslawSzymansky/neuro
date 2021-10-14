4880e8ec60f881f5481f994a1e7a24879791b7e1
RadoslawSzymansky <radek199@gmail.com>
2021-10-14 21:02:26 +0200

    add our clients section

--- footer.php
+++ footer.php
@@ -1,4 +1,32 @@
-   
+
+    <div class="container container-xl">
+      <div class="contact-footer" style="background-image: url('<?php echo get_field('contact-footer_img', 'options')['url']; ?>');">
+          <img class="contact-footer__img" src="<?php echo get_field('contact-footer_img', 'options')['url']; ?>" alt="<?php echo get_field('contact-footer_img', 'options')['alt']; ?>">
+          <div class="contact-footer__content">
+            <div>
+              <?php the_field('contact-footer_text', 'options'); ?>
+            </div>
+            <?php $contactLink = get_field('contact-footer_btn', 'options'); ?>
+            <a href="<?php echo $contactLink['url']; ?>" target="<?php echo $contactLink['target']; ?>" class="btn btn--primary btn--transparent">
+              <?php echo $contactLink['title']; ?>
+            </a>
+          </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="our-partners">
+        <div class="label"><?php the_field('our-parnerts_label'); ?></div>
+        <h2><?php the_field('our-parnerts_title'); ?></h2>
+        <div class="our-partners__row">
+          <img class="our-partners__img" src="<?php echo get_field('our-parnerts_img-1')['url']; ?>" alt="partner logo">
+          <div class="line"></div>
+          <img class="our-partners__img" src="<?php echo get_field('our-parnerts_img-2')['url']; ?>" alt="partner logo">
+        </div>
+      </div>
+    </div>
+
+
     <footer class="footer">
 
     </footer>
--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -177,28 +177,4 @@
     </div>
   </div>
 
-  <div class="contact-home-container">
-    <div class="contact-home" style="background-image: url('<?php echo get_field('contact-home_img')['url']; ?>');">
-        <div class="contact-home__content">
-          <div>
-            <?php the_field('contact-home_text'); ?>
-          </div>
-          <?php $contactLink = get_field('contact-home_btn'); ?>
-          <a href="<?php echo $contactLink['url']; ?>" target="<?php echo $contactLink['target']; ?>" class="btn btn--primary">
-            <?php echo $contactLink['title']; ?>
-          </a>
-        </div>
-    </div>
-  </div>
-
-
-  <div class="container">
-    <div style="background-color: red; height: 200px;">NORMAL</div>
-  </div>
-  <div class="container container-lg">
-    <div style="background-color: blue; height: 200px;">SMALL</div>
-  </div>
-  <div class="container container-xl">
-    <div style="background-color: green; height: 200px;">LARGE</div>
-  </div>
 <?php get_footer(); ?>
