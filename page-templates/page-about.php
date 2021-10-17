6430102a2f4eda3f5f62d3e75f6c999124b91149
RadoslawSzymansky <radek199@gmail.com>
2021-10-17 11:16:36 +0200

    add slider to about page

--- page-templates/page-about.php
+++ page-templates/page-about.php
@@ -78,5 +78,84 @@
 
   </div>
 
+  <div class="news">
+    <div class="news__wrapper">
+      <div class="news__intro">
+        <div class="label"><?php the_field('news_label'); ?></div>
+        <h2><?php the_field('news_title'); ?></h2>
+        <div class="news-nav">
+          <button><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/arrow.svg" /></button>
+          <button><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/arrow.svg" /></button>
+        </div>
+      </div>
+      <!--  -->
+      <div class="slider-wrapper">
+        <div class="news-slider">
+        <?php
+        if( have_rows('news') ):
+          while ( have_rows('news') ) : the_row(); ?>
+          <?php $item = get_sub_field('post'); ?>
+          <!-- single slide -->
+          <div>
+            <div class="news__item">
+              <a href="<?php  echo $item->guid; ?>" class="title-wrapper">
+                <h3><?php  echo $item->post_title; ?></h3>
+                <p class="short-desc"><?php the_field('short_desc', $item->ID); ?></p>
+              </a>
+              <a href="<?php  echo $item->guid; ?>" class="get-more">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/arrow-small.svg">
+                <?php pll_e('Dowiedz się więcej'); ?>
+              </a>
+            </div>
+          </div>
+          <?php endwhile;
+        else :
+        endif;
+        ?>
+          <?php
+        if( have_rows('news') ):
+          while ( have_rows('news') ) : the_row(); ?>
+          <?php $item = get_sub_field('post'); ?>
+          <!-- single slide -->
+          <div>
+            <div class="news__item">
+              <a href="<?php  echo $item->guid; ?>" class="title-wrapper">
+                <h3><?php  echo $item->post_title; ?></h3>
+              </a>
+              <a href="<?php  echo $item->guid; ?>" class="get-more">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/arrow-small.svg">
+                <?php pll_e('Dowiedz się więcej'); ?>
+              </a>
+            </div>
+          </div>
+          <?php endwhile;
+        else :
+        endif;
+        ?>
+         <?php
+        if( have_rows('news') ):
+          while ( have_rows('news') ) : the_row(); ?>
+          <?php $item = get_sub_field('post'); ?>
+          <!-- single slide -->
+          <div>
+            <div class="news__item">
+              <a href="<?php  echo $item->guid; ?>" class="title-wrapper">
+                <h3><?php  echo $item->post_title; ?></h3>
+              </a>
+              <a href="<?php  echo $item->guid; ?>" class="get-more">
+              <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/arrow-small.svg">
+                <?php pll_e('Dowiedz się więcej'); ?>
+              </a>
+            </div>
+          </div>
+          <?php endwhile;
+        else :
+        endif;
+        ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 <?php get_footer(); ?>
